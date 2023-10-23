79b9789a2276a4eefedd487852e01896a3bfcc0d
kalenio <kalenio@proton.me>
2023-10-23 10:40:35 -0700

    removed shebang

--- dotfile-git-backups.sh
+++ dotfile-git-backups.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 git init --bare $HOME/.dotfile-git-backups/
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfile-git-backups/ --work-tree=$HOME'
 echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfile-git-backups/ --work-tree=$HOME'" >> $HOME/.bashrc
