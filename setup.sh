940c9c7665f5a10fda11338e23567446871cb716
kalenio <kalenio@proton.me>
2023-10-23 10:59:25 -0700

    added directory change line to fix 'command not found' error with  dotfiles alias

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 git init --bare $HOME/.dotfile-git-backups/
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfile-git-backups/ --work-tree=$HOME'
 echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfile-git-backups/ --work-tree=$HOME'" >> $HOME/.bashrc
+cd $HOME/.dotfile-git-backups
 dotfiles config --local status.showUntrackedFiles no
