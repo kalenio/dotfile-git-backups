c498b286e2dbd66dcbdac574ffe82a32839bd9e2
kalenio <kalenio@proton.me>
2023-10-23 12:57:55 -0700

    Major rework to the script
    Removed the first line relating to the alias due to the script functioning fine without it. Added a full path to the 'git' executable in the config line, which fixed the "command not found" error when executing as a script. Code now works as intended in both script and manual executions. Added comments for each line.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+
+# Creates a folder in $HOME as a bare git repository.
 git init --bare $HOME/.dotfile-git-backups/
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfile-git-backups/ --work-tree=$HOME'
+
+# Adds the 'dotfiles' alias to the shell configuration file so it can be used in place of the 'git' command.
 echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfile-git-backups/ --work-tree=$HOME'" >> $HOME/.bashrc
-(cd $HOME/.dotfile-git-backups; dotfiles config --local status.showUntrackedFiles no)
+
+# Sets the new repository to hide any files in $HOME that we are not backing up.
+/usr/bin/git --git-dir=$HOME/.dotfile-git-backups/ --work-tree=$HOME config --local status.showUntrackedFiles no
+
