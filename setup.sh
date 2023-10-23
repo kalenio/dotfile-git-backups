5605ff77afbe03cab199c29a85897824de5bf2c6
kalenio <kalenio@proton.me>
2023-10-23 11:08:33 -0700

    moved cd line to a subshell with the 'dotfiles status.showUntrackedFiles no' command

--- setup.sh
+++ setup.sh
@@ -1,5 +1,4 @@
 git init --bare $HOME/.dotfile-git-backups/
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfile-git-backups/ --work-tree=$HOME'
 echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfile-git-backups/ --work-tree=$HOME'" >> $HOME/.bashrc
-cd $HOME/.dotfile-git-backups
-dotfiles config --local status.showUntrackedFiles no
+(cd $HOME/.dotfile-git-backups; dotfiles config --local status.showUntrackedFiles no)
